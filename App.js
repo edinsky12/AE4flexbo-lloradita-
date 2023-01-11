88f6947fcbe54d8dc80b416131b2a8d32d91557e
edinsky12 <91280755+edinsky12@users.noreply.github.com>
2023-01-11 16:16:41 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import { useTheme } from 'react-native-paper';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+//const theme = useTheme();
+
+// Agafem l'amplaria de la nostra pantalla
+const screenWidth = Dimensions.get('window').width;
+// Agafem l'alçada de la nostra pantalla
+const screenHeight = Dimensions.get('window').height;
+//Calculem el tamany de cada imatge
+const imgWidth = screenHeight > screenWidth ? screenWidth / 4 : screenHeight / 4;
+//Declarem theme per a utilitzar temes de llibreria paper
+
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.artWork}>
+            <View style={styles.square}><Text>Ací va una imatge</Text></View>
+        </View>
+        <View style={styles.titolCantant}></View>
+        <View style={styles.controls}></View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:6,
+    backgroundColor: '#191414',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderColor:'purple',
+    borderWidth:5,
+  },
+  square: {
+    width: 200,
+    height: 200,
+    backgroundColor: "#1DB954",
+    alignContent: 'flex-end'
+  },
+  imatgePortada:{
+
+  },
+  artWork:{
+    flex:3,
+    borderColor:'blue',
+    borderWidth:5,
+    justifyContent: 'center',
+    //color: theme.colors.surface
+
+  },
+  titolCantant:{
+    flex:1,
+    borderColor:'green',
+    borderWidth:5,
+  },
+  controls:{
+    flex:2,
+    borderColor:'red',
+    borderWidth:5,
+  }
+})
